d2b231651f97438c39e2fe4fb1e40821ed7ea8ab
ruigomesbioinf <rfpgomes99@gmail.com>
2022-09-24 11:36:37 +0100

    [ADD] DFA

--- bioworkbench/patternfinding.py
+++ bioworkbench/patternfinding.py
@@ -43,16 +43,25 @@ def search_all_occurrences(seq: str, pattern: str) -> List[int]:
     return res
 
 class BoyerMoore:
+    """Heuristic algorithm for pattern searching on strings. This algorithm is based on two
+    rules to make it work, which are implemented on this class
+    """
     def __init__(self, alphabet: str, pattern: str) -> None:
         self.alphabet = alphabet
         self.pattern = pattern
         self.preprocess()
 
     def preprocess(self):
+        """A function called by the class constructor that does the pre-+rocessing of both rules.
+        """
         self.preprocess_bcr()
         self.preprocess_gsr()
 
     def preprocess_bcr(self):
+        """Implementation of the Bad Character Rule, that ensures that we can advance our pattern
+        to the next occurence (in the pattern) of the symbol in the sequence at the position of 
+        the mismatch.
+        """
         self.occ = {}
         for symb in self.alphabet:
             self.occ[symb] = -1
@@ -62,6 +71,9 @@ class BoyerMoore:
         return self.occ
 
     def preprocess_gsr(self):
+        """Implementation of the Good Suffix Rule, that ensures that when a mismatch occurs we can move
+        to the next instance in the pattern of the part (suffix) that matched before (in the right) the mismatch.
+        """
         self.f = [0] * (len(self.pattern) + 1)
         self.s = [0] * (len(self.pattern) + 1)
         i = len(self.pattern)
@@ -81,6 +93,9 @@ class BoyerMoore:
             if i == j: j = self.f[j]
 
     def search_pattern(self, sequence: str) -> List[int]:
+        """Functions that allows the user to search over target sequences for a given pattern
+        using the boyer-moore rules.
+        """
         res = []
         i = 0
         while i <= len(sequence) - len(self.pattern):
@@ -93,3 +108,81 @@ class BoyerMoore:
                 c = sequence[j+i]
                 i += max(self.s[j+1], j - self.occ[c])
         return res
+
+class Automata:
+    """Class that implements the Deterministic Finite Automata for pattern searching
+    """
+    def __init__(self, alphabet: str, pattern: str) -> None:
+        self.alphabet = alphabet
+        self.numstates = len(pattern) + 1
+        self.transition_table = {}
+        self.build_transition_table(pattern)
+        
+    def build_transition_table(self, pattern: str) -> None:
+        """Function called by the constructor that creates the transition table
+
+        Args:
+            pattern (str): pattern of search
+        """
+        for x in range(self.numstates):
+            for char in self.alphabet:
+                prefix = pattern[0:x] + char
+                self.transition_table[(x, char)] = self.overlap(prefix, pattern)
+                
+    def overlap(self, s1: str, s2: str) -> int:
+        """Function that provides the maximum overlap between two sequences s1 and s2.
+
+        Args:
+            s1 (str): sequence 1
+            s2 (str): sequence 2
+
+        Returns:
+            int: maximum overlap integer
+        """
+        max_overlap = min(len(s1), len(s2))
+        for i in range(max_overlap, 0, -1):
+            if s1[-i:] == s2[:i]:
+                return i
+        return 0
+    
+    def print_automata (self):
+        """Function that allows the user to print information about the automata and the transition table
+        """
+        print(f"States: {self.numstates}")
+        print(f"Alphabet: {self.alphabet}")
+        print("Transition table: ")
+        for k in self.transition_table.keys():
+            print(f"{k[0]}, {k[1]} -> {self.transition_table[k]}")
+            
+    def next_state(self, current, symbol):
+        return self.transition_table.get((current, symbol))
+    
+    def apply_seq(self, seq: str):
+        """Function that computes a list of states the DFA goes through when processinf one sequence.
+        """
+        q = 0
+        res = [q]
+        for char in seq:
+            q = self.next_state(q, char)
+            res.append(q)
+        return res
+    
+    def occurrences_pattern(self, seq: str) -> List[int]:
+        """Computes a list of the positions of the pattern in the sequence
+
+        Args:
+            seq (str): Sequence
+
+        Returns:
+            List[int]: List of positions
+        """
+        q = 0
+        res = []
+        for i in range(len(seq)):
+            q = self.next_state(1, seq[i])
+            if q == self.numstates-1:
+                res.append(i - self.numstates + 2)
+                
+        return res
+        
+    
